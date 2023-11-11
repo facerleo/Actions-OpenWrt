996eacdd880d3c197d457b166baa375e826a5708
facerleo <86334632+facerleo@users.noreply.github.com>
2023-11-11 12:44:13 +0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,3 @@
-sed -i 's/192.168.1.1/192.168.0.86/g' package/base-files/files/bin/config_generate
-sed -i "s/OpenWrt /Tlan compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/192.168.1.1/192.168.0.86/g' package/base-files/files/bin/config_generate                                                         # 修改IP地址
+sed -i "s/OpenWrt /Tlan compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings    # 增加个性名字
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings                                                 # 设置密码为空
