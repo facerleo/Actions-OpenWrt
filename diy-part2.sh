5a51ab517773cca9b941db8cbc763653bb75ae9f
facerleo <86334632+facerleo@users.noreply.github.com>
2024-11-02 14:57:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,5 +3,5 @@ sed -i 's/192.168.1.1/192.168.5.30/g' package/base-files/files/bin/config_genera
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings     # 设置密码为空
 
-# Modify hostname
-sed -i 's/OpenWrt/Tlany-Router/g' package/base-files/files/bin/config_generate
+
+
