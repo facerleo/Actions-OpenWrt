be1ae2a75a2cfb0522f0dd6b09a938b7e1d68b19
facerleo <86334632+facerleo@users.noreply.github.com>
2025-01-25 12:23:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #
 ZZZ="package/lean/default-settings/files/zzz-default-settings"
 
-sed -i 's/192.168.1.1/192.168.5.30/g' package/base-files/files/bin/config_generate             # 修改IP地址为192.168.0.55
+sed -i 's/192.168.1.1/192.168.5.30/g' package/base-files/luci2/bin/config_generate          # 修改IP地址为192.168.0.55
 
 sed -i "s/OpenWrt /Tlany compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ         # 增加个性名字 faceroo
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                         # 设置密码为空
